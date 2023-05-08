f16d278754c5765795002ffbc0df762fe860e6b9
Łukasz Gola <lukaszgolaofficial@gmail.com>
2023-05-09 01:29:44 +0200

    login

--- trade.py
+++ trade.py
@@ -5,7 +5,7 @@ import customtkinter
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("green")
 
-class MyFrame(customtkinter.CTkFrame):
+class LoginForm(customtkinter.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
 
@@ -13,20 +13,21 @@ class MyFrame(customtkinter.CTkFrame):
         self.LoginTitle = customtkinter.CTkLabel(self, text="Zaloguj się!", font=("Roboto", 25))
         self.LoginTitle.grid(row=0,column=0,padx=20,pady=20)
 
-        self.email = customtkinter.CTkEntry(self, placeholder_text="Email",width=300,height=50,border_width=1,corner_radius=10)
+        self.email = customtkinter.CTkEntry(self, placeholder_text="Email",width=300,height=50,border_width=1,corner_radius=10, font=("Roboto", 14))
         self.email.grid(row=1,column=0,padx=20,pady=5)
 
-        self.password = customtkinter.CTkEntry(self, placeholder_text="Password",width=300,height=50,border_width=1,corner_radius=10)
+        self.password = customtkinter.CTkEntry(self, placeholder_text="Hasło",width=300,height=50,border_width=1,corner_radius=10, show="*", font=("Roboto", 14))
         self.password.grid(row=2, column=0,padx=20,pady=5)
 
         self.zaloguj = customtkinter.CTkButton(self, text="ZALOGUJ", font=("Roboto", 16), fg_color="#0DCB81", width=300, height=50)
         self.zaloguj.grid(row=3, column=0,padx=20,pady=5)
 
-        self.forgotPassword = customtkinter.CTkButton(self, text="Zapomniałeś hasła?", font=("Roboto", 12), fg_color="transparent", width=300, height=20, hover="disable")
-        self.forgotPassword.grid(row=4, column=0,padx=20,pady=5)
 
-        self.forgotPassword = customtkinter.CTkButton(self, text="Zarejestruj się?", font=("Roboto", 12), fg_color="transparent", width=300, height=20, text_color="#0DCB81", hover="disable")
-        self.forgotPassword.grid(row=5, column=0,padx=20,pady=0)
+        self.forgotPassword = customtkinter.CTkLabel(self, text="Nie masz jeszcze konta?", font=("Roboto", 12), fg_color="transparent", width=150, height=20)
+        self.forgotPassword.grid(row=4, column=0,padx=20,pady=5, sticky=tk.W)
+
+        self.forgotPassword = customtkinter.CTkButton(self, text="Zarejestruj się?", font=("Roboto", 12), fg_color="transparent", width=150, height=20, text_color="#0DCB81", hover="disable")
+        self.forgotPassword.grid(row=4, column=0,padx=20,pady=5, sticky=tk.E)
 
 class App(customtkinter.CTk):
     def __init__(self):
@@ -37,7 +38,7 @@ class App(customtkinter.CTk):
         frame1.pack(fill=None, expand=False)
 
         # add widgets to app
-        self.my_frame = MyFrame(self, fg_color="#161A1E")
+        self.my_frame = LoginForm(self, fg_color="#161A1E")
         self.my_frame.place(relx=0.5, rely=0.5,anchor=tk.CENTER)
         
     # add methods to app
